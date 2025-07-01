3bf166460459746be86ece6b930f645b8e27b2b4
Gabriel ML <gabrielmljob@gmail.com>
2025-07-01 14:24:25 +0200

    feat: Add Botoncito
    Add Botoncito Component.
    Add Botoncito styles.
    Include Botoncito component in App.jsx

--- react/primeros-ejercicios/vite-project/src/App.jsx
+++ react/primeros-ejercicios/vite-project/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Botoncito from './components/botoncito/Botoncito.jsx'
 
 function App() {
   return (
     <>
       <h1>Hello World!</h1>
+      <Botoncito/>
     </>
   )
 }
---
+++ react/primeros-ejercicios/vite-project/src/components/botoncito/Botoncito.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Botoncito.css'
+
+export const Botoncito = () => {
+  return (
+    <button className="botoncito">Mi primer Botoncito</button>
+  )
+}
+
+export default Botoncito
