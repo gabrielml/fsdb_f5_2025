d985a6ee0e8b6916c6d993e88ee5a9728cbb53d6
Gabriel ML <gabrielmljob@gmail.com>
2025-07-02 15:39:56 +0200

    feat: Add function to randomly change button's color using "State"

--- react/primeros-ejercicios/vite-project/src/components/botoncito/Botoncito.jsx
+++ react/primeros-ejercicios/vite-project/src/components/botoncito/Botoncito.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Botoncito.css'
 
 // TODO: (?) Is it a godd practice to write functions outside the Component? In this case, Button.
@@ -7,9 +7,28 @@ const printMsg = (btnNum) => {
 }
 
 export const Botoncito = (props) => {
+  // State to store the hex color value
+  const [color, setColor] = useState("#4CAF50"); // Initial color
+
+  // Generate a random 6-digit hex string
+  const randomColor = () => {
+    // Math.random() generates a pseudo-random floating-point number between 0 (inclusive) and 1 (exclusive).
+    // .toString(16) converts the random number into its hexadecimal (base 16) string representation.
+    // .slice(-6) extracts the last 6 characters from the string.
+    // TODO: Improve this statement because it could partially work, but with a significant flaw...
+    const newHexColor = "#" + (Math.random().toString(16).slice(-6));
+    console.log(newHexColor);
+    setColor(newHexColor);
+}
 
   return (
-    <button className="botoncito" onClick={() => {printMsg(props.number)}}>Mi primer Botoncito {props.number}</button>
+    <button 
+      className="botoncito"
+      style = {{ backgroundColor: color }} 
+      onClick={() => {printMsg(props.number); randomColor()}}
+    >
+      Mi primer Botoncito {props.number}
+    </button>
   )
 }
 
