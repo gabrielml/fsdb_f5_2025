e8fe6650fe01b641f81be9019c129ac6b4d9604d
Gabriel ML <gabrielmljob@gmail.com>
2025-07-02 14:15:22 +0200

    feat: Add onClick React event to Botoncito

--- react/primeros-ejercicios/vite-project/src/components/botoncito/Botoncito.jsx
+++ react/primeros-ejercicios/vite-project/src/components/botoncito/Botoncito.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './Botoncito.css'
 
+// TODO: (?) Is it a godd practice to write functions outside the Component? In this case, Button.
+const printMsg = () => {
+  console.log("You just pressed the button!");
+}
+
 export const Botoncito = (props) => {
+
   return (
-    <button className="botoncito">Mi primer Botoncito {props.number}</button>
+    <button className="botoncito" onClick={printMsg}>Mi primer Botoncito {props.number}</button>
   )
 }
 
