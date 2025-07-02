4af553b75b45ffa6ea7b3aa080ee9c435854bb17
Gabriel ML <gabrielmljob@gmail.com>
2025-07-02 14:24:10 +0200

    feat: Add property "btnNum" to the "printMsg" function
    It should print the button number when pressed.

--- react/primeros-ejercicios/vite-project/src/components/botoncito/Botoncito.jsx
+++ react/primeros-ejercicios/vite-project/src/components/botoncito/Botoncito.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import './Botoncito.css'
 
 // TODO: (?) Is it a godd practice to write functions outside the Component? In this case, Button.
-const printMsg = () => {
-  console.log("You just pressed the button!");
+const printMsg = (btnNum) => {
+  console.log(`You just pressed the button ${btnNum} !`);
 }
 
 export const Botoncito = (props) => {
 
   return (
-    <button className="botoncito" onClick={printMsg}>Mi primer Botoncito {props.number}</button>
+    <button className="botoncito" onClick={() => {printMsg(props.number)}}>Mi primer Botoncito {props.number}</button>
   )
 }
 
