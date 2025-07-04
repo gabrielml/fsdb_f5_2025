16104e515a4e330e0d68fab54976b33e00de2747
Gabriel ML <gabrielmljob@gmail.com>
2025-07-04 12:20:20 +0200

    feat: Update Card component
    -[x] Include three new properties.
    -[x] Completed structure.

--- react/primeros-ejercicios/vite-project/src/components/card/Card.jsx
+++ react/primeros-ejercicios/vite-project/src/components/card/Card.jsx
@@ -1,22 +1,33 @@
+// import "./Card.css";
+
 const user = {
-  name: 'Hedy Lamarr',
-  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
-  imageSize: 90,
+  name: "Hedy Lamarr",
+  title: "Actress & Inventor",
+  about:
+    "Hedy Lamarr was an Austrian-born American actress and inventor. After a brief early film career in Czechoslovakia, including the controversial erotic romantic drama Ecstasy, she fled from her first husband, Friedrich Mandl, and secretly moved to Paris.",
+  imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
+  imageSize: 125,
 };
 
 export default function Card() {
   return (
     <>
-      <h1>{user.name}</h1>
-      <img
-        className="card"
-        src={user.imageUrl}
-        alt={'Photo of ' + user.name}
-        style={{
-          width: user.imageSize,
-          height: user.imageSize
-        }}
-      />
+      <div className="profile-card">
+        <img
+          className="profile-img"
+          src={user.imageUrl}
+          alt={"Photo of " + user.name}
+          style={{
+            width: user.imageSize,
+            height: user.imageSize,
+          }}
+        />
+        <div className="profile-info">
+          <h1 className="profile-name">{user.name}</h1>
+          <h2 className="profile-title">{user.title}</h2>
+          <p className="profile-about">{user.about}</p>
+        </div>
+      </div>
     </>
   );
 }
