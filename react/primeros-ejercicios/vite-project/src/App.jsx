c8dd4b75ea852db28394ed21145883a2ce0c3492
Gabriel ML <gabrielmljob@gmail.com>
2025-07-04 14:28:37 +0200

    feat: Add two more Card to render
    Update Card component to accept props.
    Add three user objects to App.jsx [Hardcode]
    Add two more Card tags to App.jsx [Hardcode]
    
    TODO: store user data in an array and use map() to render the Card
    components dinamically!

--- react/primeros-ejercicios/vite-project/src/App.jsx
+++ react/primeros-ejercicios/vite-project/src/App.jsx
@@ -1,18 +1,69 @@
-import './App.css'
-import Botoncito from './components/Botoncito/Botoncito.jsx'
-import Card from './components/Card/Card.jsx'
+import "./App.css";
+import Botoncito from "./components/Botoncito/Botoncito.jsx";
+import Card from "./components/Card/Card.jsx";
 
 function App() {
+  const user1 = {
+    name: "Hedy Lamarr",
+    title: "Actress & Inventor",
+    about:
+      "Hedy Lamarr was an Austrian-born American actress and inventor. After a brief early film career in Czechoslovakia, including the controversial erotic romantic drama Ecstasy, she fled from her first husband, Friedrich Mandl, and secretly moved to Paris.",
+    imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
+    imageSize: 125,
+  };
+
+  const user2 = {
+    name: "Maria Sklodowska-Curie",
+    title: "Physicist & Chemist",
+    about:
+      "Marie Skłodowska Curie was a Polish and naturalized French physicist and chemist who conducted pioneering research on radioactivity. She was the first woman to win a Nobel Prize, the first person and only woman to win the Nobel Prize twice, and the only person to win the Nobel Prize in two different scientific fields.",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/c/c8/Marie_Curie_c._1920s.jpg",
+    imageSize: 125,
+  };
+
+  const user3 = {
+    name: "Ada Lovelace",
+    title: "Mathematician & Writer",
+    about:
+      "Augusta Ada King, Countess of Lovelace was an English mathematician and writer, chiefly known for her work on Charles Babbage's proposed mechanical general-purpose computer, the Analytical Engine. She was the first to recognise that the machine had applications beyond pure calculation, and published the first algorithm intended to be carried out by such a machine.",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/a/a4/Ada_Lovelace_portrait.jpg",
+    imageSize: 125,
+  };
+
   return (
     <>
       <h1>Hello World!</h1>
-      <Botoncito number="1"/>
-      <Botoncito number="2"/>
-      <Botoncito number="3"/>
-      <Botoncito number="4"/>
-      <Card/>
+      <Botoncito number="1" />
+      <Botoncito number="2" />
+      <Botoncito number="3" />
+      <Botoncito number="4" />
+      
+      {/* TODO: Use an advance approach to store the user data in an array and use the map() function to render the Card components dynamically! */}
+      <Card
+        name={user1.name}
+        title={user1.title}
+        about={user1.about}
+        imageUrl={user1.imageUrl}
+        imageSize={user1.imageSize}
+      />
+      <Card
+        name={user2.name}
+        title={user2.title}
+        about={user2.about}
+        imageUrl={user2.imageUrl}
+        imageSize={user2.imageSize}
+      />
+      <Card
+        name={user3.name}
+        title={user3.title}
+        about={user3.about}
+        imageUrl={user3.imageUrl}
+        imageSize={user3.imageSize}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- react/primeros-ejercicios/vite-project/src/components/card/Card.jsx
+++ react/primeros-ejercicios/vite-project/src/components/card/Card.jsx
@@ -1,31 +1,31 @@
 import "./Card.css";
 
-const user = {
-  name: "Hedy Lamarr",
-  title: "Actress & Inventor",
-  about:
-    "Hedy Lamarr was an Austrian-born American actress and inventor. After a brief early film career in Czechoslovakia, including the controversial erotic romantic drama Ecstasy, she fled from her first husband, Friedrich Mandl, and secretly moved to Paris.",
-  imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
-  imageSize: 125,
-};
+// const user = {
+//   name: "Hedy Lamarr",
+//   title: "Actress & Inventor",
+//   about:
+//     "Hedy Lamarr was an Austrian-born American actress and inventor. After a brief early film career in Czechoslovakia, including the controversial erotic romantic drama Ecstasy, she fled from her first husband, Friedrich Mandl, and secretly moved to Paris.",
+//   imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
+//   imageSize: 125,
+// };
 
-export default function Card() {
+export default function Card({name, title, about, imageUrl, imageSize}) {
   return (
     <>
       <div className="profile-card">
         <img
           className="profile-img"
-          src={user.imageUrl}
-          alt={"Photo of " + user.name}
+          src={imageUrl}
+          alt={"Photo of " + name}
           style={{
-            width: user.imageSize,
-            height: user.imageSize,
+            width: imageSize,
+            height: imageSize,
           }}
         />
         <div className="profile-info">
-          <h1 className="profile-name">{user.name}</h1>
-          <h2 className="profile-title">{user.title}</h2>
-          <p className="profile-about">{user.about}</p>
+          <h1 className="profile-name">{name}</h1>
+          <h2 className="profile-title">{title}</h2>
+          <p className="profile-about">{about}</p>
         </div>
       </div>
     </>
