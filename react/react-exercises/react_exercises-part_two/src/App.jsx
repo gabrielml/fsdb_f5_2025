4020c9eae3d7ad1b2aaf658d1686b643ad357186
Gabriel ML <gabrielmljob@gmail.com>
2025-07-14 19:12:24 +0200

    feat: Add global styles to App

--- react/react-exercises/react_exercises-part_two/src/App.jsx
+++ react/react-exercises/react_exercises-part_two/src/App.jsx
@@ -4,9 +4,12 @@ import { Stopwatch } from './components/Stopwatch/Stopwatch'
 function App() {
 
   return (
-    <div className="app-container">
-      <h1>Welcome to React exercises part 2</h1>
-      <Stopwatch/>
+    <div className="app">
+      <h1 className='app__title'>Welcome to React exercises part 2</h1>
+      <div className='app__container'>
+        <Stopwatch/>
+        {/* <ReactionGame/> */}
+      </div>
     </div>
   )
 }
